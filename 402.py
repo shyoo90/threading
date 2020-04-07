39455d237de2e5d32f04b777b8b7dd1d5adbf3ae
Lyla <lyla@iacryl.com>
2020-04-07 16:58:40 +0900

    'fourth commit'

--- 402.py
+++ 402.py
@@ -9,50 +9,52 @@ import time
 
 app = Flask(__name__)
 
-que = queue.Queue(maxsize=2)
+que = queue.Queue(maxsize=4)
 lock1 = threading.RLock()
 lock2 = threading.RLock()
 lock_que = threading.Lock()
 condition1 = threading.Condition(lock1)
 condition2 = threading.Condition(lock2)
 iter = 0
+notif = 0
 
 @app.route("/c", methods=["GET","POST"])
 def c():
     global que
+    global notif
+
     if request.method == 'POST':
         data = request.get_json()
         data = list(data.values())[0]
         if que.full():
             with condition1:
                 condition1.wait()
-            with condition2:
-                que.put(data)
-                condition2.notify()
-        else:
-            with condition2:
-                que.put(data)
-                condition2.notify()
+
+        with lock_que:
+            que.put(data)
+
+        with condition2:
+            condition2.notify()
+            notif+=1
+            print(notif, file = sys.stderr)
     return ''
 
 def d():
     global que
     global iter
+
     while True:
-        if que.full():
-            with condition1:
-                a = que.get()
-                iter +=1
-                print(a, iter, file=sys.stderr)
-                condition1.notify()
-        else:
+        if que.empty():
             with condition2:
                 condition2.wait()
-            with condition1:
-                a = que.get()
-                iter +=1
-                print(a, iter,  file=sys.stderr)
-                condition1.notify()
+
+        with lock_que:
+            a = que.get()
+            iter +=1
+            print(a, iter, file=sys.stderr)
+        
+        with condition1:
+            condition1.notify()
 
 
 thread = threading.Thread(target = d, daemon=True)
---
+++ past.py
@@ -0,0 +1,65 @@
+from flask import Flask, request
+import threading
+import queue
+from concurrent.futures import ThreadPoolExecutor
+import atexit
+import sys
+import time
+
+
+app = Flask(__name__)
+
+que = []
+condition1 = threading.Condition()
+condition2 = threading.Condition()
+lock = threading.Lock()
+iter = 0
+
+@app.route("/c", methods=["GET","POST"])
+def c():
+    global que
+    with lock:
+        if request.method == 'POST':
+            data = request.get_json()
+            data = list(data.values())[0]
+
+            if len(que)>=4:
+                with condition1:
+                    condition1.wait()
+                with condition2:
+                    que.append(data)
+                    condition2.notify()
+
+            else:
+                with condition2:
+                    que.append(data)
+                    condition2.notify()
+    return ''
+
+def d():
+    global que
+    global iter
+    while True:
+        if len(que)>0:
+            with condition1:
+                a = que[0]
+                que.remove(a)
+                iter +=1
+                print(a, iter, file=sys.stderr)
+                condition1.notify()
+
+        else:
+            with condition2:
+                condition2.wait()
+                a = que[0]
+                que.remove(a)
+                iter +=1
+                print(a, iter,  file=sys.stderr)
+
+
+thread = threading.Thread(target = d, daemon=True)
+thread.start()
+
+if __name__ == "__main__":
+
+    app.run('0.0.0.0',8080, threaded = True)
